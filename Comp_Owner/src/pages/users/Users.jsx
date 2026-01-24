5f01d934d35e4aee89717bd0697ddd2ca047208a
Vipin <vipinbhardwal286@gmail.com>
2026-01-24 23:02:55 +0530

    users page

---
+++ Comp_Owner/src/pages/users/Users.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+// import { getAllUsers } from "../api/ownerApi";
+
+import "./user.css";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     getAllUsers()
+//       .then(res => setUsers(res.data))
+//       .catch(err => console.log(err));
+//   }, []);
+
+  return (
+    <div className="users">
+      <h1 className="users-title">Users</h1>
+
+      <div className="users-table-wrapper">
+        <table className="users-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {users.map(user => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td className="role">{user.role}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {users.length === 0 && (
+          <p className="no-data">No users found</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
