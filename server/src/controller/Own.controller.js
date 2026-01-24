75c61beeebdab2d1d2e1ce9e47e30c07d5d79484
Vipin <vipinbhardwal286@gmail.com>
2026-01-24 16:27:10 +0530

    quiz find by creator ID

--- server/src/controller/Own.controller.js
+++ server/src/controller/Own.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import Quiz from "../models/Quiz.js";
+
 
 export const Owner_GetAllUsers = async(req ,res)=>{
     try {
@@ -15,6 +17,15 @@ export const Owner_GetAllCreator = async(req ,res)=>{
         const data = await User.find({role:"creator"});
         res.send(data)
       
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const Owner_GetAllQuizzes = async(req ,res)=>{
+    try {
+        const data = await Quiz.find();
+        res.send(data)
     } catch (error) {
         console.log(error)
     }
--- server/src/routes/owner.routes.js
+++ server/src/routes/owner.routes.js
@@ -1,13 +1,24 @@
 import express from "express";
 
-
-import {  Owner_GetAllUsers , Owner_GetAllCreator} from "../controller/Own.controller.js";
-
+import Quiz from "../models/Quiz.js";
+import { Owner_GetAllUsers, Owner_GetAllCreator, Owner_GetAllQuizzes } from "../controller/Own.controller.js";
 
 const Router = express.Router();
 
-Router.get("/all-users",Owner_GetAllUsers);
-Router.get("/all-creator",Owner_GetAllCreator);
+Router.get("/all-users", Owner_GetAllUsers);
+Router.get("/all-creator", Owner_GetAllCreator);
+Router.get("/all-quizzes", Owner_GetAllQuizzes);
+
+Router.get("/creator-quiz", async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        // const data = await Quiz.find({ createdBy:userId})
+        const data = await Quiz.find({ createdBy: "695932b3aef3f3825c43ce93" })
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
+});
 
 export default Router;
 
