bd9ba4d6bfa9a50e50c0292ce783104362f101e1
Vipin <vipinbhardwal286@gmail.com>
2026-01-25 19:35:36 +0530

    quiz page done

--- Comp_Owner/src/pages/Quizzes/Quizzes.jsx
+++ Comp_Owner/src/pages/Quizzes/Quizzes.jsx
@@ -1,25 +1,111 @@
-import { Link } from "react-router-dom";
-import { getAllQuizzes } from "../../utils/API";
+// import { Link } from "react-router-dom";
+// import { getQuizBytech } from "../../utils/API";
+// import { useEffect, useState } from "react";
+// import "./quizzes.css";
+
+// const Quizzes = () => {
+//   const [data, setData] = useState([]);
+//   const [tech, setTech] = useState("HTML");
+//   const [filteredQuizzes, setTilteredQuizzes] = useState([]);
+//   const [showTech, setShowTech] = useState(5);
+//   const [creatorOfQuiz, setCreatorOfQuiz] = useState("");
+
+//   useEffect(() => {});
+
+//   useEffect(() => {
+//     getQuizBytech(tech)
+//       .then((res) => setData(res.data.data))
+//       .catch((error) => console.log(error));
+//   }, [tech]);
+
+//   useEffect(() => {
+//     const filtered = data.filter((q) => q.tech === tech);
+//     setTilteredQuizzes(filtered);
+//   }, [tech, data]);
+
+//   const technologies = [
+//     "HTML",
+//     "CSS",
+//     "JavaScript",
+//     "React",
+//     "Node.js",
+//     "Express",
+//     "MongoDB",
+//     "C++ Programming",
+//     "C Programming",
+//     "Java Programming",
+//   ];
+
+//   return (
+//     <>
+//       <div className="all-quizzes">
+//         <h2>Quizzes</h2>
+
+//         <div className="technologies-box">
+//           <ul>
+//             {technologies.slice(0, showTech).map((tech) => (
+//               <li key={tech} onClick={() => setTech(tech)}>
+//                 {tech}
+//               </li>
+//             ))}
+//             <p
+//               className="moreButton"
+//               onClick={() => {
+//                 showTech === 5
+//                   ? setShowTech(technologies.length)
+//                   : setShowTech(5);
+//               }}
+//             >
+//               {showTech === 5 ? ">" : " "}
+//             </p>
+//           </ul>
+//         </div>
+//         <h3>Technology name: {tech}</h3>
+//         {filteredQuizzes.map((quiz) => (
+//           <div key={quiz._id}>
+//             <p>{quiz.title}</p>
+//             <p>Total Questions: {quiz.questions.length}</p>
+//             <p>created By: {quiz.createdBy.name}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Quizzes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { useEffect, useState } from "react";
+import { getQuizBytech } from "../../utils/API";
 import "./quizzes.css";
 
-
 const Quizzes = () => {
-  const [data, setData] = useState([]);
-  const [tech, setTech] = useState("HTML");
-  const [filteredQuizzes, setTilteredQuizzes] = useState([]);
+  const [quizzes, setQuizzes] = useState([]);
+  const [selectedTech, setSelectedTech] = useState("HTML");
   const [showTech, setShowTech] = useState(5);
-
-  useEffect(() => {
-    getAllQuizzes()
-      .then((res) => setData(res.data))
-      .catch((error) => console.log(error));
-  }, []);
-
-  useEffect(() => {
-    const filtered = data.filter((q) => q.tech === tech);
-    setTilteredQuizzes(filtered);
-  }, [tech, data]);
 
   const technologies = [
     "HTML",
@@ -29,116 +115,62 @@ const Quizzes = () => {
     "Node.js",
     "Express",
     "MongoDB",
-    "C++ Programming",
     "C Programming",
     "Java Programming",
   ];
 
+  useEffect(() => {
+    const fetchQuizzes = async () => {
+      try {
+        const res = await getQuizBytech(selectedTech);
+        setQuizzes(res?.data?.data || []);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchQuizzes();
+  }, [selectedTech]);
 
   return (
-    <>
-      <div className="all-quizzes">
-        <h2>Quizzes</h2>
+    <div className="all-quizzes">
+      <h2>Quizzes</h2>
 
-        <div className="technologies-box">
-          <ul>
-            {technologies.slice(0, showTech).map((tech) => (
-              <li key={tech} onClick={() => setTech(tech)}>
-                {tech}
-              </li>
-            ))}
-            <p  className="moreButton"
-              onClick={() => {
-                showTech === 5
-                  ? setShowTech(technologies.length)
-                  : setShowTech(5);
-              }}
+      <div className="technologies-box">
+        <ul>
+          {technologies.slice(0, showTech).map((tech) => (
+            <li
+              key={tech}
+              className={selectedTech === tech ? "active" : ""}
+              onClick={() => setSelectedTech(tech)}
             >
-              {showTech === 5 ? ">" : "X"}
-            </p>
+              {tech}
+            </li>
+          ))}
+          <span
+            className="moreButton"
+            onClick={() =>
+              setShowTech(showTech === 5 ? technologies.length : 5)
+            }
+          >
+            {showTech === 5 ? ">" : "<"}
+          </span>
+        </ul>
+      </div>
 
+      <h3>Technology name: {selectedTech}</h3>
 
-            {/* <li
-              onClick={() => {
-                setTech("HTML");
-              }}
-            >
-              HTML
-            </li>
-            <li
-              onClick={() => {
-                setTech("CSS");
-              }}
-            >
-              CSS
-            </li>
-            <li
-              onClick={() => {
-                setTech("JavaScript");
-              }}
-            >
-              JAVASCRIPT
-            </li>
-            <li
-              onClick={() => {
-                setTech("React");
-              }}
-            >
-              REACT.JS
-            </li>
-            <li
-              onClick={() => {
-                setTech("Node.js");
-              }}
-            >
-              NODE.JS
-            </li>
-            <li
-              onClick={() => {
-                setTech("Express");
-              }}
-            >
-              EXPRESS.JS
-            </li>
-            <li
-              onClick={() => {
-                setTech("MongoDB");
-              }}
-            >
-              MONGODB
-            </li>
-            <li
-              onClick={() => {
-                setTech("C Programming");
-              }}
-            >
-              C Programming
-            </li>
-            <li
-              onClick={() => {
-                setTech("C++ Programming");
-              }}
-            >
-              C++ Programming
-            </li>
-            <li
-              onClick={() => {
-                setTech("Java Programming");
-              }}
-            >
-              JAVA Programming
-            </li> */}
-          </ul>
-        </div>
-        <h3>Technology name: {tech}</h3>
-        {filteredQuizzes.map((quiz) => (
-          <div key={quiz._id}>
-            <p>{quiz.title}</p>
-            <p>Total Questions: {quiz.questions.length}</p>
+      <div className="quiz-list">
+        {quizzes.map((quiz) => (
+          <div className="quiz-card" key={quiz._id}>
+            <p className="quiz-title">Title: {quiz.title}</p>
+            <p className="total-qus">Total Questions: {quiz.questions?.length}</p>
+            <p className="createdBy">Created By: {quiz.createdBy?.name}</p>
+           <button className="visit-button">visit quiz</button>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
