0541809fbfd425643b65ba50374ff17c9a28d560
Vipin <vipinbhardwal286@gmail.com>
2026-01-27 23:45:51 +0530

    api:method added getQuizGrowthData, getTechWiseQuizData

--- Comp_Owner/src/utils/API.js
+++ Comp_Owner/src/utils/API.js
@@ -4,6 +4,9 @@ export const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
 });
 
+export const getToalNoUserQuizCreators = () => api.get('/owner/total');
+export const getQuizGrowthData = () => api.get('/owner/QuizGrowth');
+export const getTechWiseQuizData = () => api.get('/owner/TechWiseQuizCount');
 export const getAllUsers = () => api.get('/owner/all-users');
 export const getAllCreators = () => api.get('/owner/all-creator');
 export const getAllQuizzes = () => api.get('/owner/all-quizzes');
