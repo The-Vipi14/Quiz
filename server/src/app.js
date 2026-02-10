ed122d61f85a739175e4845ecacb107aabfab7a7
Vipin <vipinbhardwal286@gmail.com>
2026-02-10 22:21:28 +0530

    cors updated

--- server/src/app.js
+++ server/src/app.js
@@ -9,12 +9,19 @@ import ownerRoutes from './routes/owner.routes.js'
 
 const app = express();
 
-app.use(cors({
+import cors from "cors";
+
+app.use(
+  cors({
     origin: [
-        "http://localhost:5173",
-        "http://localhost:5174"
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://codify-mwjo.onrender.com" // ✅ frontend Render URL
     ],
-}));
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 
